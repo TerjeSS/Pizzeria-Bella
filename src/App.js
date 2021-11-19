d41f881e0259a164115389ce3bca8587f876d4d1
StianOek <66445281+StianOek@users.noreply.github.com>
2021-11-19 14:00:44 +0100

    footer fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage/LandingPage';
-import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
-import MenuPage from './pages/MenuPage/MenuPage';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import { Container, Grid, Link, TextField } from '@material-ui/core';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import MenuPage from "./pages/MenuPage/MenuPage";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import { Container, Grid, Link, TextField } from "@material-ui/core";
 
 function App() {
-	return (
-		<div>
-			<Navbar />
-			<Breadcrumbs aria-label="breadcrumb">
-				<Link href="/landingpage">Home</Link>
-				<Link href="/">Menu</Link>
-			</Breadcrumbs>
-			<Routes>
-				<Route path="/landingPage" element={<LandingPage />} />
-				<Route path="/checkout" element={<CheckoutPage />} />
-				<Route path="/" element={<MenuPage />} />
-			</Routes>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
