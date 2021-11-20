ae18fd63e79f3d44a135c85955059e989d3f0229
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-11-20 16:16:22 +0100

    layoutComplete

--- src/components/checkout/Payment.js
+++ src/components/checkout/Payment.js
@@ -1,15 +1,6 @@
 import React from "react";
 import PaymentStyle from "./PaymentStyle";
-import {
-  Typography,
-  Container,
-  Box,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  FormControl,
-  TextField,
-} from "@mui/material";
+import { Container, Box, TextField, Button } from "@mui/material";
 import visaImg from "./img/visa.png";
 import vippsImg from "./img/vipps_logo_rgb.png";
 import paypalImg from "./img/paypal.png";
@@ -17,10 +8,70 @@ import paypalImg from "./img/paypal.png";
 export default function Payment() {
   const style = PaymentStyle();
   return (
-    <Box className={style.rowBox}>
-      <img className={style.pngIcon} src={visaImg} alt="" />
-      <img className={style.pngIcon} src={vippsImg} alt="" />
-      <img className={style.pngIcon} src={paypalImg} alt="" />
-    </Box>
+    <Container className={style.container}>
+      <Box className={style.rowBox}>
+        <img className={style.pngIcon} src={visaImg} alt="" />
+        <img className={style.pngIcon} src={vippsImg} alt="" />
+        <img className={style.pngIcon} src={paypalImg} alt="" />
+      </Box>
+      <Box className={style.textFieldBox}>
+        <Box component="form">
+          <TextField
+            style={{
+              margin: "5px 0",
+              width: "100%",
+              backgroundColor: "#f4f4f4",
+            }}
+            required
+            id="outlined-basic"
+            label="Kortholders navn"
+            variant="outlined"
+          />
+          <TextField
+            style={{
+              margin: "5px 0",
+              width: "100%",
+              backgroundColor: "#f4f4f4",
+            }}
+            required
+            id="outlined-basic"
+            label="Kortnummer"
+            variant="outlined"
+          />
+          <TextField
+            style={{
+              margin: "5px 0",
+              width: "20%",
+              backgroundColor: "#f4f4f4",
+            }}
+            required
+            id="outlined-basic"
+            label="CVC"
+            variant="outlined"
+          />
+          <TextField
+            style={{
+              margin: "5px 0",
+              width: "80%",
+              backgroundColor: "#f4f4f4",
+            }}
+            required
+            id="outlined-basic"
+            label="Utløpsdato"
+            variant="outlined"
+          />
+          <Button
+            variant="contained"
+            style={{
+              margin: "30px 0",
+              backgroundColor: "#0F2841",
+              padding: "10px 30px",
+            }}
+          >
+            Checkout
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
--- src/components/checkout/PaymentStyle.js
+++ src/components/checkout/PaymentStyle.js
@@ -1,10 +1,12 @@
 import { makeStyles } from "@mui/styles";
 
 const PaymentStyle = makeStyles((theme) => ({
-  columnBox: {
-    display: "flex",
-    flexDirection: "column",
-    margin: "auto",
+  container: {
+    backgroundColor: "#f5f5f5",
+  },
+  textFieldBox: {
+    backgroundColor: "#c4c4c4",
+    padding: "50px",
   },
   rowBox: {
     display: "flex",
@@ -13,6 +15,10 @@ const PaymentStyle = makeStyles((theme) => ({
   },
   pngIcon: {
     height: "60px",
+  },
+  btn: {
+    margin: "30px 0",
+    backgroundColor: "#0F2841",
   },
 })); // returns an object that contains all the styles
 
