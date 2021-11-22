96d8be9e9f09f83fe0c1385d8a4c912995ae9fda
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-11-22 16:09:53 +0100

    quantity state works

--- src/App.js
+++ src/App.js
@@ -1,71 +1,82 @@
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage/LandingPage';
-import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
-import MenuPage from './pages/MenuPage/MenuPage';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import GlobalStyles from './GlobalStyles';
-import Login from './pages/Login/Login';
-import Products from './Products';
-import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import MenuPage from "./pages/MenuPage/MenuPage";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import GlobalStyles from "./GlobalStyles";
+import Login from "./pages/Login/Login";
+import Products from "./Products";
+import { useState } from "react";
 
 function App() {
-	//Attempt nr1 for AddToCart
-	const [
-		shoppingCart,
-		setShoppingCart
-	] = useState([]);
+  //Attempt nr1 for AddToCart
+  const [shoppingCart, setShoppingCart] = useState([]);
 
-	const addToCart = (e, quant, storrelse) => {
-		console.log('quant=' + quant);
-		const id = e.currentTarget.getAttribute('value');
-		let tempArray = Products.filter((product) => product.id == id);
+  const addToCart = (e, quant, storrelse) => {
+    console.log("quant=" + quant);
+    const id = e.currentTarget.getAttribute("value");
+    let tempArray = Products.filter((product) => product.id == id);
 
-		//for å legge inn riktig pris
-		tempArray[0].selectedSize = storrelse;
-		tempArray[0].selectedQuantity = quant;
-		if (tempArray[0].selectedSize === 'liten') {
-			tempArray[0].realPrice = tempArray[0].priceSmall;
-		}
-		else if (tempArray[0].selectedSize === 'stor') {
-			tempArray[0].realPrice = tempArray[0].priceLarge;
-		}
-		const newItem = { ...tempArray[0] };
+    //for å legge inn riktig pris
+    tempArray[0].selectedSize = storrelse;
+    tempArray[0].selectedQuantity = quant;
+    if (tempArray[0].selectedSize === "liten") {
+      tempArray[0].realPrice = tempArray[0].priceSmall;
+    } else if (tempArray[0].selectedSize === "stor") {
+      tempArray[0].realPrice = tempArray[0].priceLarge;
+    }
+    const newItem = { ...tempArray[0] };
 
-		setShoppingCart((prevState) => {
-			return [
-				...prevState,
-				newItem
-			];
-		});
+    const exists = shoppingCart.find((item) => item.id === tempArray[0].id);
 
-		console.log(shoppingCart);
-	};
+    const equalSize = shoppingCart.find(
+      (item) => item.selectedSize === tempArray[0].selectedSize
+    );
+    if (exists && equalSize) {
+      shoppingCart.map((item) =>
+        item.id === tempArray[0].id &&
+        item.selectedSize === tempArray[0].selectedSize
+          ? (item.selectedQuantity += tempArray[0].selectedQuantity)
+          : null
+      );
+    } else {
+      setShoppingCart((prevState) => {
+        return [...prevState, newItem];
+      });
+    }
 
-	const [
-		quantity,
-		setQuantity
-	] = useState(0);
+    console.log(shoppingCart);
+  };
 
-	return (
-		<div>
-			<GlobalStyles />
-			<Navbar shoppingCart={shoppingCart} />
+  const [quantity, setQuantity] = useState(0);
 
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/checkout" element={<CheckoutPage shoppingCart={shoppingCart} />} />
-				<Route
-					path="/menu"
-					element={
-						<MenuPage shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} addToCart={addToCart} />
-					}
-				/>
-				<Route path="/login" element={<Login />} />
-			</Routes>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <GlobalStyles />
+      <Navbar shoppingCart={shoppingCart} />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/checkout"
+          element={<CheckoutPage shoppingCart={shoppingCart} />}
+        />
+        <Route
+          path="/menu"
+          element={
+            <MenuPage
+              shoppingCart={shoppingCart}
+              setShoppingCart={setShoppingCart}
+              addToCart={addToCart}
+            />
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/checkout/SelectedCard.js
+++ src/components/checkout/SelectedCard.js
@@ -1,57 +1,74 @@
-import React from 'react';
-import SelectedCardStyle from './SelectedCardStyle';
+import React from "react";
+import SelectedCardStyle from "./SelectedCardStyle";
 // material ui components:
-import { Typography, Card, CardContent, CardMedia, Grid, Box, IconButton } from '@mui/material';
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Box,
+  IconButton,
+} from "@mui/material";
 // icons and png
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import glutenImg from './img/gluten.png';
-import milkImg from './img/milk-bottle.png';
-import soyaImg from './img/soya.png';
-import trashImg from './img/trash.png';
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import glutenImg from "./img/gluten.png";
+import milkImg from "./img/milk-bottle.png";
+import soyaImg from "./img/soya.png";
+import trashImg from "./img/trash.png";
 
 export default function SelectedCard(props) {
-	const style = SelectedCardStyle();
-	const { shoppingCart } = props;
+  const style = SelectedCardStyle();
+  const { shoppingCart } = props;
 
-	return (
-		<Grid container spacing={3}>
-			{shoppingCart.map((product) => (
-				<Grid item key={product} xs={12} sm={12} md={6} lg={6}>
-					<Card className={style.card} style={{ backgroundColor: '#E9EDF0', borderRadius: '5%' }}>
-						<CardMedia className={style.cardMedia} image={product.image} title="Image title" />
+  return (
+    <Grid container spacing={3}>
+      {shoppingCart.map((product) => (
+        <Grid item key={product} xs={12} sm={12} md={6} lg={6}>
+          <Card
+            className={style.card}
+            style={{ backgroundColor: "#E9EDF0", borderRadius: "5%" }}
+          >
+            <CardMedia
+              className={style.cardMedia}
+              image={product.image}
+              title="Image title"
+            />
 
-						<CardContent className={style.cardContent}>
-							<Typography gutterBottom variant="h6">
-								{product.name}
-							</Typography>
-							<Typography variant="subtitle2">Pris: {product.realPrice};</Typography>
-						</CardContent>
+            <CardContent className={style.cardContent}>
+              <Typography gutterBottom variant="h6">
+                {product.name}
+              </Typography>
+              <Typography variant="subtitle2">
+                Pris: {product.realPrice};
+              </Typography>
+            </CardContent>
 
-						<Box className={style.columnBox}>
-							<Box>
-								<img src={glutenImg} className={style.pngIcon} />
-								<img src={soyaImg} className={style.pngIcon} />
-								<img src={milkImg} className={style.pngIcon} />
-							</Box>
-							<Box className={style.rowBox}>
-								<IconButton aria-label="add" size="small">
-									<AddCircleOutlineIcon />
-								</IconButton>
-								<Typography>{product.selectedQuantity}</Typography>
-								<IconButton aria-label="add" size="small">
-									<RemoveCircleOutlineIcon />
-								</IconButton>
-							</Box>
-						</Box>
-						<Box className={style.delete}>
-							<IconButton aria-label="delete">
-								<img src={trashImg} className={style.deleteIcon} />
-							</IconButton>
-						</Box>
-					</Card>
-				</Grid>
-			))}
-		</Grid>
-	);
+            <Box className={style.columnBox}>
+              <Box>
+                <img src={glutenImg} className={style.pngIcon} />
+                <img src={soyaImg} className={style.pngIcon} />
+                <img src={milkImg} className={style.pngIcon} />
+              </Box>
+              <Box className={style.rowBox}>
+                <IconButton aria-label="add" size="small">
+                  <AddCircleOutlineIcon />
+                </IconButton>
+                <Typography>{product.selectedQuantity}</Typography>
+                <IconButton aria-label="add" size="small">
+                  <RemoveCircleOutlineIcon />
+                </IconButton>
+              </Box>
+            </Box>
+            <Box className={style.delete}>
+              <IconButton aria-label="delete">
+                <img src={trashImg} className={style.deleteIcon} />
+              </IconButton>
+            </Box>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
