69e18e69fc84a7fea2a88967e66356f48a0f55ea
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-11-23 16:21:06 +0100

    good job

--- src/components/checkout/SelectedCardStyle.js
+++ src/components/checkout/SelectedCardStyle.js
@@ -1,37 +1,37 @@
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from '@mui/styles';
 
 const SelectedCardStyle = makeStyles((theme) => ({
-  card: {
-    margin: "10px",
-    height: "100px",
-    minWidth: "300px",
-    display: "flex",
-    border: "1px solid #0F2841",
-  },
-  cardMedia: {
-    height: "100%",
-    minWidth: "28%",
-    objectFit: "cover",
-  },
-  columnBox: {
-    display: "flex",
-    flexDirection: "column",
-    margin: "auto",
-  },
-  rowBox: {
-    display: "flex",
-    alignItems: "center",
-    marginTop: "10px",
-  },
-  delete: {
-    margin: "auto",
-  },
-  pngIcon: {
-    height: "18px",
-  },
-  deleteIcon: {
-    height: "38px",
-  },
+	card       : {
+		margin   : '10px',
+		height   : '100%',
+		minWidth : '300px',
+		display  : 'flex',
+		border   : '1px solid #0F2841'
+	},
+	cardMedia  : {
+		height    : '100%',
+		minWidth  : '28%',
+		objectFit : 'cover'
+	},
+	columnBox  : {
+		display       : 'flex',
+		flexDirection : 'column',
+		margin        : 'auto'
+	},
+	rowBox     : {
+		display    : 'flex',
+		alignItems : 'center',
+		marginTop  : '10px'
+	},
+	delete     : {
+		margin : 'auto'
+	},
+	pngIcon    : {
+		height : '18px'
+	},
+	deleteIcon : {
+		height : '38px'
+	}
 })); // returns an object that contains all the styles
 
 export default SelectedCardStyle;
